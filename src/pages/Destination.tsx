0d49fed73fb2ca8ca978a54886929bccbb44b42f
Parves Hossain Rabby <parves.pe.wb.h@gmail.com>
2024-02-04 22:31:49 +0600

    destination animation complete

--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -21,6 +21,13 @@ const Destination = () => {
       animate={{
         x: 0,
       }}
+      exit={{
+        opacity: 0,
+        transition: {
+          delay: 1.4,
+          duration: 0.2,
+        },
+      }}
       transition={{
         duration: 0.5,
       }}
@@ -28,22 +35,33 @@ const Destination = () => {
     >
       <NavbarSpace />
       <div className="mx-auto w-full max-w-[1110px] px-6 pb-14 pt-2.5 md:pb-16 md:pt-10 xl:px-0 xl:pt-8">
-        <h5 className="heading-5 flex justify-center gap-4 md:justify-start">
+        <motion.h5
+          initial={{ opacity: 0, y: "100%" }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: "-100%" }}
+          transition={{ duration: 0.3, delay: 0.5 }}
+          className="heading-5 flex justify-center gap-4 md:justify-start"
+        >
           <span className="font-bold opacity-25">01</span>
           Pick your destination
-        </h5>
+        </motion.h5>
         <div className="mt-8 flex flex-col items-center gap-6 md:mt-[60px] md:gap-14 lg:flex-row lg:items-end lg:justify-between xl:pl-16">
-          <div className="">
-            <img
+          <div>
+            <motion.img
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -50 }}
+              transition={{ duration: 0.3, delay: 0.5 }}
               className="size-[170px] md:size-[300px] lg:size-[445px]"
               src={`/destination/${currentDestination.images.webp}`}
               alt={currentDestination.name}
+              key={currentDestination.name}
             />
           </div>
           <div>
             <div className="flex justify-center gap-6 md:gap-9 lg:justify-start">
               {destinations.map((destination, index) => (
-                <button
+                <motion.button
                   key={destination.name}
                   className={twMerge(
                     "relative py-2 font-barlow-condensed font-normal uppercase tracking-[2.36px] text-secondary duration-300 hover:text-white",
@@ -52,34 +70,82 @@ const Destination = () => {
                       : "",
                   )}
                   onClick={() => changeDestination(index)}
+                  initial={{ opacity: 0, y: "100%" }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: "-100%" }}
+                  transition={{
+                    duration: 0.3,
+                    delay: 0.5 + index * 0.1,
+                    ease: "easeIn",
+                  }}
                 >
                   {destination.name}
                   {currentDestination.name === destination.name && (
                     <motion.span
                       className="absolute bottom-0 left-0 right-0 h-[3px] bg-white"
                       layoutId="destinationUnderline"
+                      transition={{
+                        type: "spring",
+                        bounce: 0.2,
+                        duration: 0.6,
+                      }}
                     ></motion.span>
                   )}
-                </button>
+                </motion.button>
               ))}
             </div>
             <div className="mt-5 max-w-[330px] text-center md:max-w-[580px] lg:max-w-[450px] lg:text-left">
-              <h2 className="heading-2">{currentDestination.name}</h2>
-              <p className="body-text leading-[1.75] text-secondary">
+              <motion.h2
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -50 }}
+                transition={{ duration: 0.3, delay: 0.5 }}
+                className="heading-2"
+                key={`destinationTitle-${currentDestination.name}`}
+              >
+                {currentDestination.name}
+              </motion.h2>
+              <motion.p
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -50 }}
+                transition={{ duration: 0.3, delay: 0.7 }}
+                className="body-text leading-[1.75] text-secondary"
+                key={`destinationDescription-${currentDestination.name}`}
+              >
                 {currentDestination.description}
-              </p>
-              <hr className="my-8 border-[#383B4B] md:mt-12 lg:mt-14" />
+              </motion.p>
+              <motion.hr
+                initial={{ scaleX: 0 }}
+                animate={{ scaleX: "100%" }}
+                exit={{ scaleX: 0 }}
+                transition={{ duration: 0.3, delay: 0.9 }}
+                className="my-8 border-[#383B4B] md:mt-12 lg:mt-14"
+                key={`line-${currentDestination.name}`}
+              />
               <div className="flex flex-col items-center justify-center gap-8 md:flex-row md:gap-24 lg:justify-start lg:gap-20">
-                <div>
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -50 }}
+                  transition={{ duration: 0.3, delay: 0.9 }}
+                  key={`destinationStats-${currentDestination.name}`}
+                >
                   <p className="subheading-2 text-secondary">Avg. Distance</p>
                   <h4 className="heading-4">{currentDestination.distance}</h4>
-                </div>
-                <div>
+                </motion.div>
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -50 }}
+                  transition={{ duration: 0.3, delay: 1.1 }}
+                  key={`destinationTravel-${currentDestination.name}`}
+                >
                   <p className="subheading-2 text-secondary">
                     Est. Travel Time
                   </p>
                   <h4 className="heading-4">{currentDestination.travel}</h4>
-                </div>
+                </motion.div>
               </div>
             </div>
           </div>
